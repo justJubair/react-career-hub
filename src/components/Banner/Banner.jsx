07b8dc9ff38ecba37f6883d50bbdd632e189557d
Jubair <jubair.ahmed2838@gmail.com>
2023-09-23 14:00:34 +0600

    Add: show all button

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
       <div>
         <h1 className="text-5xl leading-tight font-bold">One Step <br /> Closer To Your <br /> <span>Dream Job</span></h1>
         <p className="py-6">Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
-        <button className="btn btn-primary">Get Started</button>
+        <button className="btn text-white bg-gradient-to-r from-[#7E90FE] to-[#9873FF]">Get Started</button>
       </div>
     </div>
   </div>
--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,20 +5,34 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
+   const [dataLength, setDataLength] = useState(4)
+   const [isShowAll, setIsShowAll] = useState(true)
+   
     useEffect(()=>{
         fetch('jobs.json')
         .then(res => res.json())
         .then(data => setJobs(data))
-    },[])
-    const handleShowAll = ()=>{
       
+    },[])
+
+    const handleShowAll =()=>{
+      if(isShowAll){
+        setDataLength(jobs.length)
+      } else{
+        setDataLength(4)
+      }
+      setIsShowAll(!isShowAll)
     }
+   
+   
   return (
     <div>
       <div className="grid grid-cols-1 gap-8 md:grid-cols-2 mt-24">
-        {jobs.map(job=> <Job key={job.id} job={job}/>)}
+        {jobs.slice(0, dataLength).map(job=> <Job key={job.id} job={job}/>)}
       </div>
-      <button onClick={handleShowAll} className="btn-primary btn">show all</button>
+    <div className="text-center my-6">
+    <button onClick={handleShowAll} className="btn text-white bg-gradient-to-r from-[#7E90FE] to-[#9873FF]">{isShowAll ? 'Show all' : 'show less'}</button>
+    </div>
     </div>
   )
 }
